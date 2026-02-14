703b0df10092b701365375da3fabca34badcf984
gabaissues <gabaissues@gmail.com>
2026-02-14 13:20:51 -0300

    now, accept enviroment variable NEXT_PUBLIC_API_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.gabrieljesus.dev",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "https://api.gabrieljesus.dev",
   headers: {
     Authorization: `Bearer ${process.env.API_TOKEN || "tst"}`,
   },
